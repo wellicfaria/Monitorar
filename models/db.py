7e8535b5bee509d8efbfa645149f9eadade63a16
Wellington C. Faria <wellington@spin.agr.br>
2015-11-05 14:51:41 -0200

    Banco mysql

--- models/db.py
+++ models/db.py
@@ -20,7 +20,7 @@ if request.is_local:
     db = DAL('sqlite://storage.db')
 else:
     #/Quando o sistema esta no servidor do pythonanywhere, o mesmo utiliza mysql
-    db = DAL('mysql://monitorar:ar123@monitorar.mysql.pythonanywhere-services.com/$default')
+    db = DAL('mysql://monitorar:ar123@monitorar.mysql.pythonanywhere-services.com/monitorar$monitorar')
 
 
 
